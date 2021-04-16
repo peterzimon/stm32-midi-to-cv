0e03e7f430b85934d7b70f809bb439e4e7d95c7e
Peter Zimon <peter.zimon@gmail.com>
2021-04-16 22:09:21 +0200

    Updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 /**
- * MIDI to 4 polyphonic CV out on STM32F103C8 using HAL libraries (mostly).
- * Uses Shmoergh stm32lib: https://github.com/peterzimon/stm32lib
+ * MIDI to 4 polyphonic CV out 
+ * ---------------------------
+ * Platform: STM32F103 + PlatformIO using ST LINK V2 for programming
+ * Framework: STM32CUBE
+ * Hardware: MCP4728 DAC + standard MIDI Input using 6N138 optoisolator
+ * Optional: UART/TTL converter for serial debug
+ * 
+ * Uses Shmoergh stm32lib: https://github.com/peterzimon/stm32lib. These
+ * libraries must be added to .vscode/c_cpp_properties.json includePath 
+ * and browse/path.
 */
 
 #include <string.h>
